9d7a22697e41dca2e3dbe749bafe1d23853257a1
karim.maw <karimmawlawi96@gmail.com>
2023-08-05 17:42:31 +0300

    fixed the redirecting issue

--- components/btns/LoginBtn.tsx
+++ components/btns/LoginBtn.tsx
@@ -11,7 +11,7 @@ export default function LoginBtn({session}: {session: Session | null}) {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${location.origin}/auth/callback`,
       },
     });
   }
